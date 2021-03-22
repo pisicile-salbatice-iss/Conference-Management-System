ff19e2af06975421506134b659d92a2ecbc91488
WildUrti <46956225+WildUrti@users.noreply.github.com>
2021-03-22 20:31:55 +0200

    added service field to view

--- src/main/kotlin/gui/views/LoginView.kt
+++ src/main/kotlin/gui/views/LoginView.kt
@@ -4,6 +4,7 @@ import javafx.scene.control.Alert
 import javafx.scene.control.Button
 import javafx.scene.control.TextField
 import javafx.scene.layout.GridPane
+import service.Service
 import tornadofx.View
 import tornadofx.action
 import tornadofx.alert
@@ -13,8 +14,10 @@ class LoginView : View("CMS") {
     override val root : GridPane by fxml()
     val usernameField : TextField by fxid()
     val passwordField : TextField by fxid()
-    val loginButton: Button by fxid()
-    val createAccountButton: Button by fxid()
+    private val loginButton: Button by fxid()
+    private val createAccountButton: Button by fxid()
+
+    private val service = Service()
 
     init {
         loginButton.apply { action { handleLogin() }}
